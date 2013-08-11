3a7043605a5bea36078734143d975c4d91dbd512
lznew <support@lonniezanenewkirk.com>
2013-08-11 11:15:10 -0500

    Info file uploaded

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: SchApp001
+Plugin URI: https://github.com/lznew/schapp001
+Description: Scheduling app
+Author: lznew
+Author URI: http://lonniezanenewkirk.com
+Version: 0.1-alpha
+*/
